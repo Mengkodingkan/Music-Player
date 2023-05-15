be985bc468eadac9dbd9fac39d42a921ab222d56
Marli <marleysumarly@gmail.com>
2023-05-15 14:41:42 +0700

    Update

--- frontend/src/app/users/account/account.page.ts
+++ frontend/src/app/users/account/account.page.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {IonModal} from "@ionic/angular";
+import {OverlayEventDetail} from '@ionic/core/components';
 
 @Component({
   selector: 'app-account',
@@ -7,10 +9,30 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AccountPage implements OnInit {
 
+  @ViewChild(IonModal) modal: any;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string = '';
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
   }
 
 }
