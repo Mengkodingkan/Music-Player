c1ddc7dc7142e53683dbcc4b328b20cf568276ce
Kato <naufalfaqih443@gmail.com>
2023-06-02 22:13:12 +0700

    partial update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::get('/phpinfo', function () {
+    return phpinfo();
+});
 
 Route::get('/', function () {
     return response()->json([
